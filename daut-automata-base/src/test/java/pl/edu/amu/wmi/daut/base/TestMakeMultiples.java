21b9814a7338dfc483cd98b2dddb28890857564a
rafip141 <rafi_p141@wp.pl>
2011-12-20 16:08:10 +0100

    ble2.1

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestMakeMultiples.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestMakeMultiples.java
@@ -14,19 +14,9 @@ public class TestMakeMultiples extends TestCase {
     public final void test1() {
        final AutomatonSpecification spec = new NaiveAutomatonSpecification();
 
-	spec.makeMultiples("a",5);
-	final AutomatonByStack automaton = new AutomatonByStack(spec);
-	
-        State q0a = spec.addState();
-        State q1a = spec.addState();
-        State q2a = spec.addState();
-        State q3a = spec.addState();
-        State q4a = spec.addState();
-        State q5a = spec.addState();
-     
-
+	final AutomatonByStack automaton = new AutomatonByStack(spec.makeMultiples(a,5););
+	      
         assertTrue(automaton.accepts(""));
-
         assertTrue(automaton.accepts("aaaaa"));
         assertTrue(automaton.accepts("aaaaaaaaaa"));
         assertTrue(automaton.accepts("aaaaaaaaaaaaaaa"));
@@ -37,3 +27,4 @@ public class TestMakeMultiples extends TestCase {
    }
 
 }
+
