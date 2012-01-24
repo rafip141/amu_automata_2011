f9df79e1f9d392b5ffc3dd134da49c0cba180d25
rafip141 <rafi_p141@wp.pl>
2012-01-24 18:47:47 +0100

    wersja poprawiona obejrzana

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestMakeMultiples.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestMakeMultiples.java
@@ -9,22 +9,54 @@ import junit.framework.TestCase;
 */
 public class TestMakeMultiples extends TestCase {
 /**
-* Pierwszy test.
+* Pierwszy test. Wielokrotnosci 5
 */
     public final void test1() {
        
-
-	final AutomatonByStack automaton = new AutomatonByStack(AutomatonSpecification.makeMultiples("a",5));
+        
+	final AutomatonByStack automat_pierwszy = new AutomatonByStack(AutomatonSpecification.makeMultiples('a',5));
 	      
-        assertTrue(automaton.accepts(""));
-        assertTrue(automaton.accepts("aaaaa"));
-        assertTrue(automaton.accepts("aaaaaaaaaa"));
-        assertTrue(automaton.accepts("aaaaaaaaaaaaaaa"));
-        assertFalse(automaton.accepts("aaaaaaaaaaaaaa"));
-        assertFalse(automaton.accepts("bbbbbbbbaaaaac"));
-        assertFalse(automaton.accepts("bbbbbbbbaaaaaaaaa"));
+        assertTrue(automat_pierwszy.accepts(""));
+        assertTrue(automat_pierwszy.accepts("aaaaa"));
+        assertTrue(automat_pierwszy.accepts("aaaaaaaaaa"));
+        assertTrue(automat_pierwszy.accepts("aaaaaaaaaaaaaaa"));
+        assertFalse(automat_pierwszy.accepts("aaaaaaaaaaaaaa"));
+        assertFalse(automat_pierwszy.accepts("bbbbbbbbaaaaac"));
+        assertFalse(automat_pierwszy.accepts("bbbbbbbbaaaaaaaaa"));
         
    }
-
+/**
+* Drugi test.Wielokrotnosci 1
+*/
+    public final void test2() {
+       
+        
+	final AutomatonByStack automat_drugi = new AutomatonByStack(AutomatonSpecification.makeMultiples('a',1));
+	      
+        assertTrue(automat_drugi.accepts(""));
+        assertTrue(automat_drugi.accepts("a"));
+        assertTrue(automat_drugi.accepts("aaaaaaaaaa"));
+        assertTrue(automat_drugi.accepts("aaaaaaaaaaaaaaa"));
+        assertTrue(automat_drugi.accepts("aaaaaaaaaaaaaa"));
+        assertFalse(automat_drugi.accepts("bbbbbbbbaaaaac"));
+        assertFalse(automat_drugi.accepts("bbbbbbbbaaaaaaaaa"));
+        
+   }
+/**
+* Trzeci test.Wielokrotnosci 2
+*/  
+  public final void test3() {
+       
+        
+	final AutomatonByStack automat_trzeci = new AutomatonByStack(AutomatonSpecification.makeMultiples('a',2));
+	      
+        assertTrue(automat_trzeci.accepts(""));
+        assertTrue(automat_trzeci.accepts("aa"));
+        assertTrue(automat_trzeci.accepts("aaaaaaaaa"));
+        assertTrue(automat_trzeci.accepts("aaaaaaaaaaaaaa"));
+        assertFalse(automat_trzeci.accepts("a"));
+        assertFalse(automat_trzeci.accepts("bbbbbbbbaaaaac"));
+        assertFalse(automat_trzeci.accepts("bbbbbbbbaaaaaaaaa"));
+        assertFalse(automat_trzeci.accepts("aaa"));
+   }
 }
-
